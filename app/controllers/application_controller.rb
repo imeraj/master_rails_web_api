a4b7a78fc857a9fafee69f5bd937e300bb51c2f3
Meraj <meraj.enigma@gmail.com>
2021-08-15 10:33:27 -0400

    - add query builder: paginator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  rescue_from QueryBuilderError, with: :query_builder_error
+
+  protected
+
+  def paginate(scope)
+    paginator = Paginator.new(scope, request.query_parameters, current_url)
+    response.headers["Links"] = paginator.links
+    paginator.paginate
+  end
+
+  def current_url
+    request.base_url + request.path
+  end
+
+  def query_builder_error(error)
+    render status: 400, json: {
+      error: {
+        message: error.message,
+        invalid_params: error.invalid_params
+      }
+    }
+  end
 end
