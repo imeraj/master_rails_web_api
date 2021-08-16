dd2c0a066a46663f7c9d3f8714cb7f42d97a8dfb
Meraj <meraj.enigma@gmail.com>
2021-08-16 07:54:21 -0400

    - prepare more builders

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,12 +1,17 @@
 class BasePresenter
-  @build_attributes = []
+  CLASS_ATTRIBUTES = {
+    build_with: :build_attributes,
+    related_to: :relations,
+    sort_by: :sort_attributes,
+    filter_by: :filter_attributes
+  }.freeze
+
+  CLASS_ATTRIBUTES.each { |_k, v| instance_variable_set("@#{v}", []) }
 
   class << self
-    attr_accessor :build_attributes
+    attr_accessor *CLASS_ATTRIBUTES.values
 
-    def build_with(*args)
-      @build_attributes = args.map(&:to_s)
-    end
+    CLASS_ATTRIBUTES.each { |k, v| define_method(k) { |*args| instance_variable_set("@#{v}", args.map(&:to_s)) } }
   end
 
   attr_accessor :object, :params, :data
